9d0e16779bc27a2112331a836d9e1a35ba00e672
Reed Lauber <reedlauber@gmail.com>
2013-01-14 08:30:15 -0500

    Migrated mapping to Leaflet

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,17 +1,27 @@
 class MapController < ApplicationController
-  def index
-    @back_path = "/#{@route_type}/#{@route_id}/#{@direction_id}/#{@from.stop_id}"
-    if(@to != nil)
-      @back_path += "/#{@to.stop_id}"
-    end
+	def index
+		@back_path = "/#{@route_type}/#{@route_id}/#{@direction_id}/#{@from.stop_id}"
+		if(@to != nil)
+			@back_path += "/#{@to.stop_id}"
+		end
 
-    @bus = params[:bus]
+		@bus = params[:bus]
 
-    if(params[:trip] != nil)
-      trip = Trip.where("trip_id = ?", params[:trip]).first
-      if(trip != nil)
-        @shape_pts = Shape.where("shape_id = ?", trip.shape_id).order("shape_pt_sequence")
-      end
-    end
-  end
+		if(params[:trip] != nil)
+			trip = Trip.where("trip_id = ?", params[:trip]).first
+			if(trip != nil)
+				shape_pts = Shape.where("shape_id = ?", trip.shape_id).order("shape_pt_sequence")
+
+				coordinates = []
+				shape_pts.each do |point|
+					coordinates << [point.shape_pt_lon, point.shape_pt_lat]
+				end
+
+				@coords = {
+					:type => "LineString",
+					:coordinates => coordinates
+				}
+			end
+		end
+	end
 end
--- public/javascripts/ns-map.js
+++ public/javascripts/ns-map.js
@@ -3,22 +3,16 @@
 		var _self = {},
 			_options = $.extend({
 				id: 'map',
-                baseLayers: {
-                    gphy: { label: 'Physical', layer: new OpenLayers.Layer.Google("Google Physical", { type: google.maps.MapTypeId.TERRAIN, numZoomLevels: 18 }) },
-                    gmap: { label: 'Streets', layer: new OpenLayers.Layer.Google("Google Streets", { type: google.maps.MapTypeId.ROADMAP, numZoomLevels: 20 }) },
-                    gsat: { label: 'Satellite', layer: new OpenLayers.Layer.Google("Google Satellite", { type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22 }) },
-                    ghyb: { label: 'Hybrid', layer: new OpenLayers.Layer.Google("Google Hybrid", { type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20 }) }
-                },
-                zoom: 16,
-                center: { lat:39.9523350, lng:-75.163789 } // Philadelphia
+				zoom: 16,
+				center: { lat:39.9523350, lng:-75.163789 } // Philadelphia
 			}, options),
 			_manager,
 			_zoom = _options.zoom;
 
-		var _map, _proj, _mapProj, 
-			_vectorLayer, _markerLayer, _markerShadowLayer,
-			_bus, _user,
-			_markers = {
+		var _map,
+			_vehicleMarker,
+			_vehicle, _user,
+			_icons = {
 				bus: {
 					width: 32,
 					height: 37,
@@ -33,37 +27,54 @@
 
 		var $map;
 
+		var _vehicleLocated = _userLocated = false;
 		function _setExtendedLocation() {
-			if(_bus && _user) {
-				var bounds = new OpenLayers.Bounds();
-				bounds.extend(new OpenLayers.LonLat(_bus.lng, _bus.lat).transform(_proj, _map.getProjectionObject()));
-				bounds.extend(new OpenLayers.LonLat(_user.lon, _user.lat).transform(_proj, _map.getProjectionObject()));
-				_map.zoomToExtent(bounds);
-			} else if(_bus) {
-            	_setCenter(_bus.lng, _bus.lat);
+			if(_vehicle && _user) {
+				var bounds = _map.getBounds();
+				if(!_vehicleLocated) {
+					bounds.extend([_vehicle.lat, _vehicle.lng]);
+					_vehicleLocated = true;
+				}
+				if(!_userLocated) {
+					bounds.extend([_user.lat, _user.lon]);
+					_userLocated = true;
+				}
+				_map.fitBounds(bounds);
+			} else if(_vehicle) {
+				_vehicleLocated = true;
+				_setCenter(_vehicle.lng, _vehicle.lat);
 			} else if(_user) {
-            	_setCenter(_bus.lng, _bus.lat);
+				_userLocated = true;
+				_setCenter(_user.lon, _user.lat);
 			}
 		}
 
 		function _addUserLocation() {
-			_setExtendedLocation();
 			_addMarker(_user.lon, _user.lat, null, 'user');
+			_setExtendedLocation();
 		}
-		
+
 		function _updateBusLocation() {
 			var label = '',
-				offset = parseInt(_bus.Offset, 10);
+				offset = parseInt(_vehicle.Offset, 10);
 			if(!isNaN(offset)) {
 				label = offset + ' min' + (offset == 1 ? '' : 's') + ' ago';
 			}
 
-			_markerLayer.clearMarkers();
-			_setExtendedLocation();
 			if(_user) {
 				_addMarker(_user.lon, _user.lat, null, 'user');
 			}
-            _addMarker(_bus.lng, _bus.lat, label);
+
+			if(_vehicleMarker) {
+				var vehicleLatLng = _vehicleMarker.getLatLng();
+				if(vehicleLatLng.lat !== _vehicle.lat && vehicleLatLng.lng !== _vehicle.lng) {
+					_vehicleLocated = false;
+					_updateMarker(_vehicle.lng, _vehicle.lat, _vehicleMarker, label);
+				}
+			} else {
+				_addMarker(_vehicle.lng, _vehicle.lat, label);
+			}
+			_setExtendedLocation();
 		}
 
 		function _getBusLocation(routeId, vehicleId) {
@@ -75,7 +86,9 @@
 					});
 				}
 				if(vehicleId in buses) {
-					_bus = buses[vehicleId];
+					_vehicle = buses[vehicleId];
+					_vehicle.lat = parseFloat(_vehicle.lat);
+					_vehicle.lng = parseFloat(_vehicle.lng);
 					_updateBusLocation();
 					setTimeout(function() {
 						_getBusLocation(routeId, vehicleId);
@@ -87,66 +100,60 @@
 		}
 
 		function _addMarkerInfo(marker, info) {
-			var $icon = $(marker.icon.imageDiv);
-			if($icon.length) {
-				var $info = $('<div class="ns-map-markerinfo s-corner-all-4"></div>').html(info).appendTo($icon);
+			marker.unbindPopup();
+			marker.bindPopup(info, {
+				offset: L.point(57, 10),
+				closeButton: false,
+				className: 'ns-map-markerinfo s-corner-all-4'
+			});
+			marker.openPopup();
+		}
+
+		function _addMarker(x, y, info, type) {
+			var iconInfo = _icons[type || 'bus'],
+				icon = L.icon({
+					iconUrl: iconInfo.path,
+					iconSize: [iconInfo.width, iconInfo.height]
+				});
+
+			var marker = L.marker([y, x], {
+				icon: icon,
+				title: info || ''
+			}).addTo(_map);
+
+			if(type !== 'user') {
+				_vehicleMarker = marker;
+			}
+
+			if(info) {
+				_addMarkerInfo(marker, info);
 			}
 		}
 
-        function _addMarker(x, y, info, marker) {
-        	var marker = _markers[marker || 'bus'];
+		function _updateMarker(x, y, marker, label) {
+			marker.setLatLng([y, x]);
 
-            var size = new OpenLayers.Size(marker.width, marker.height),
-                offset = new OpenLayers.Pixel(-(size.w / 2), -size.h),
-                icon = new OpenLayers.Icon(marker.path, size, offset),
-                point = new OpenLayers.LonLat(x, y).transform(_proj, _map.getProjectionObject()),
-                marker = new OpenLayers.Marker(point, icon);
-
-            _markerLayer.addMarker(marker);
-
-            if(info) {
-            	_addMarkerInfo(marker, info);
-            }
-        }
-
-        function _setCenter(lng, lat, zoom) {
-        	zoom = zoom || _zoom;
-            _map.setCenter(new OpenLayers.LonLat(lng, lat).transform(_proj, _map.getProjectionObject()), zoom);
-        }
-
-		function _setupMapControls() {
-			var $zoom = $('<div class="ns-map-zoom"></div>').appendTo($map);
-			var $zoomIn = $('<a href="javascript:void(0)" data-zoom="in" class="s-shadow-small"></a>').appendTo($zoom);
-			var $zoomOut = $('<a href="javascript:void(0)" data-zoom="out" class="s-shadow-small"></a>').appendTo($zoom);
-
-			$('a', $zoom).click(function() {
-				var zoomFn = $(this).attr('data-zoom') === 'in' ? 'zoomIn' : 'zoomOut';
-				_map[zoomFn]();
-			});
+			if(label) {
+				_addMarkerInfo(marker, label);
+			}
 		}
 
+		function _setCenter(lng, lat, zoom) {
+			zoom = zoom || _zoom;
+			_map.setView([lat, lng], zoom);
+		}
 
-        function _setupOverlayLayers() {
-        	_vectorLayer = new OpenLayers.Layer.Vector("Vectors", { style: { strokeColor:'#3366DD', strokeWidth:6, strokeOpacity:0.7 } });
-            //_markerShadowLayer = new OpenLayers.Layer.Markers("Shadows");
-            _markerLayer = new OpenLayers.Layer.Markers("Markers");
+		function _setupRouteOverlay() {
+			var geoJson = $('#' + _options.id + '-shape').html();
+			if(geoJson) {
+				geoJson = JSON.parse(geoJson);
+				if(geoJson) {
+					L.geoJson(geoJson, {
 
-            _map.addLayers([_vectorLayer, _markerLayer]);
-        }
-
-        function _setupRouteOverlay() {
-        	var wkt = $('#' + _options.id + '-shape').html();
-        	if(wkt) {
-        		var wktReader = new OpenLayers.Format.WKT({
-	                'internalProjection': _map.baseLayer.projection,
-	                'externalProjection': new OpenLayers.Projection("EPSG:4326")
-	            });
-        		var feature = wktReader.read(wkt);
-        		if(feature) {
-	        		_vectorLayer.addFeatures([feature]);
-	        	}
-        	}
-        }
+					}).addTo(_map);
+				}
+			}
+		}
 
 		_self.init = function(manager, state) {
 			_manager = manager;
@@ -155,42 +162,28 @@
 			var routeId = $map.attr('data-route'),
 				vehicleId = $map.attr('data-bus');
 
-            _proj = new OpenLayers.Projection("EPSG:4326");
+			_map = L.map(_options.id + '-inner')
 
-            _map = NXS.Map = new OpenLayers.Map(_options.id + '-inner', {
-                sphericalMercator: true,
-                units: 'degrees',
-                theme: null,
-                controls: [
-					new OpenLayers.Control.Navigation()
-				]
-            });
-            _mapProj = _map.getProjectionObject();
+			L.tileLayer('http://{s}.tile.cloudmade.com/fdb4e543deef4dccbc7d4383c5f3c783/997/256/{z}/{x}/{y}.png', {
+				maxZoom: 22
+			}).addTo(_map);
 
-            _map.addLayers([
-			    _options.baseLayers.gphy.layer
-		    ]);
+			_setupRouteOverlay();
 
-			$('.olLayerDiv', $map).addClass('ns-shadow-inset');
+			_getBusLocation(state.routeId, vehicleId);
 
-            _setupMapControls();
-            _setupOverlayLayers();
-            _setupRouteOverlay();
-
-            _getBusLocation(state.routeId, vehicleId);
-
-            _setCenter(_options.center.lng, _options.center.lat, 13);
+			_setCenter(_options.center.lng, _options.center.lat, 12);
 
 			if(navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(function(position) {
 					_user = { lat:position.coords.latitude, lon:position.coords.longitude };
 					_addUserLocation();
-			    });
+				});
 			}
 
 			return _self;
 		};
-		
+
 		return _self;
 	};
 })(NextSepta);
